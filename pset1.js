9248dd885334ae9c97396973813c91f77ba5da8b
Alexander <aionate0812@gmail.com>
2018-10-06 12:52:01 -0400

    added solutions

--- pset1.js
+++ pset1.js
@@ -4,14 +4,30 @@ PROBLEM 1:
 What are the types of the following expressions and what do they evaluate to, and why?
 
 -> 17
+Integer | 17 | 
+
 -> 1 + 2 * 3 + 4
+Arithmetic Operation | 11 | 
+
 -> 800 / 80 / 8
+Arithmetic Operation | 1. 25 | 
+
 -> 400 > 200
+Comparison Operation | true | 
+
 -> 1 !== 1
+Logical Operation | false
+
 -> true || false
+Logical Operation | true
+
 -> true && false
+Logical Operation | false
+
 -> 20 % 6
+Arithmetic Operation | 2
 -> 'a' + 'b'
+Concatenation | ab
 
 ******************/
 
@@ -23,9 +39,17 @@ PROBLEM 2:
 What will the following return? Why?
 
 -> typeof 4
+Integer | is a number 
+
 -> typeof 'hello'
+String | is a word inside quotes
+
 -> typeof true
+Boolean | true is a boolean operator
+
 -> 2 === 1 || 3 === 4
+False | the result of the OR operation is false
+
 
 ******************/
 
@@ -37,11 +61,11 @@ Create a truth table for the expression A || B.
 
 For reference, here is a truth table for the expression A && B:
 
-|   A   |   B   | A && B| 
-| true  | true  | true  |
-| false | true  | false |
-| true  | false | false |
-| false | false | false | 
+|   A   |   B   | A && B| A || B
+| true  | true  | true  | true
+| false | true  | false | false 
+| true  | false | false | false
+| false | false | false | false
 
 
 ******************/
@@ -55,11 +79,11 @@ Create a truth table for the expression !A && !B.
 
 For reference, here is a truth table for the expression A && !B:
 
-|   A   |   B   |   !B   | A &&!B| 
-| true  | true  | false  | false |
-| false | true  | false  | false |
-| true  | false | true   | true  |
-| false | false |  true  | false | 
+|   A   |   B   |   !B   | A &&!B|  !A   | !A && !B
+| true  | true  | false  | false | false |  false
+| false | true  | false  | false | true  |  false
+| true  | false | true   | true  | false |  false   
+| false | false |  true  | false | true  |  true
 
 ******************/
 
@@ -76,6 +100,10 @@ For reference, here is a exp of a step-by-step evaluation:
         6 + 4
            10
 
+2 + 3 * 2 + 1
+2 + 6 + 1
+8 + 1
+9
 
 ******************/
 
@@ -86,9 +114,12 @@ PROBLEM 6:
 
 Write a step-by-step evaluation for the following expression (remember order of operations): 4 / 2 + 8 / 4.
 
+4 / 2 + 8 / 4
+2 + 8 / 4
+2 + 2
+4
 
 ******************/
-
 
 
 /******************
@@ -96,7 +127,10 @@ PROBLEM 7:
 
 Write a step-by-step evaluation for the following expression: 'ca' + 'ter' + 'pi' + 'llar'.
 
-
+'ca' + 'ter' + 'pi' + 'llar'
+'cater' + pi' + 'llar'
+'caterpi' + 'llar'
+'caterpillar'
 
 ******************/
 
@@ -106,5 +140,8 @@ PROBLEM 8:
 
 Write a step-by-step evaluation for the following expression: 2 * 4 === 8 && 'car' + 'pool' === 'carpool'.
 
+2 * 4 === 8 && 'car' + 'pool' === 'carpool'
 
 ******************/
+
+console.log(2 * 4 === 8 && 'car' + 'pool' === 'carpool')
