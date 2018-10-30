2f9201e923318267e138fde549281f1aecf3f2ec
Pamela Abreu <pamelaabreu@pursuit.com>
2018-10-30 02:20:47 -0400

    Solved problems 1 - 8

--- pset1.js
+++ pset1.js
@@ -16,7 +16,6 @@ it will solve it through the order of PEMDAS.
 This will solve it as 
 1. 2 * 3 = 6
 2. 1 + 6 + 4 = 11
-Log it as 11
 */
 
 
@@ -25,47 +24,46 @@ Log it as 11
 Solution: This will solve it as 
 1. 800/ 80 = 10, 
 2. 10/8 = 1.25
-Log it as 1.25
 */
 
 
 /*
 -> 400 > 200
-Solution: This will log it a true, bc it's testing the relationship between 400 and 200. 
-The test returns one of the two boolean values, so 400 is greater than 200, so it's logged as true.
+Solution: This will r8n true, bc it's testing the relationship between 400 and 200. 
+The test returns one of the two boolean values, so 400 is greater than 200, so it's true.
 */
 
 
 /*
 -> 1 !== 1
 Solution: !== tests is comparing if 1 is not the same as 1, since 1 is the same as 1,
-it will be logged as false. If it was 2 !== 1, it will log as true, since 2 is not the same as 1.
+it will brun false. If it was 2 !== 1, it will run true, since 2 is not the same as 1.
 */
 
 
 /*
 -> true || false
 Solution: || operator means or, it's checking if one of the statements is true. So it will
-log as true, because one of the statements is true.
+run true, because one of the statements is true.
 */
 
 
 /*
 -> true && false
-Solution: The && is checking if both statements are true. It will log as false, because
+Solution: The && is checking if both statements are true. It will run false, because
 one side of the statement is false.
 */
 
 
 /*
 -> 20 % 6
-Solution: The % is the modulo, so its finding the modulo of 20 and 6. It will log 2. 
+Solution: The % moduo operator is finding the modulo(remainder) of 20 and 6. It will run 2. 
 */
 
 
 /*
 -> 'a' + 'b'
-Solution: This will log as ab
+Solution: This will run 'ab.'
 */
 
 
@@ -75,16 +73,16 @@ PROBLEM 2:
 What will the following return? Why?
 
 -> typeof 4
-Solution : log number, bc it's date type is a number
+Solution : return number, bc it's data type is a number.
 
 -> typeof 'hello'
-Solution: string, bc it's data type is a string
+Solution: returns string, bc it's data type is a string.
 
 -> typeof true
-Solution: boolean, bc it's data type is boolean
+Solution: returns boolean, bc it's data type is boolean.
 
 -> 2 === 1 || 3 === 4
-Solution: False, because 2 is not 1, and 3 is not 4.
+Solution: returns false, because 2 is not 1, and 3 is not 4.
 
 ******************/
 
@@ -95,11 +93,11 @@ PROBLEM 3:
 Create a truth table for the expression A || B.
 Solution: 
 
-|   A   |   B   |   A || B    |
-| false | true  | true
-| true  | false | true
-| true  | true  | true
-| false | false | false
+|   A   |   B   |   A || B      |
+| false | true  |   true        |
+| true  | false |   true        |
+| true  | true  |   true        |
+| false | false |   false       |
 
 For reference, here is a truth table for the expression A && B:
 
@@ -117,10 +115,10 @@ PROBLEM 4:
 Create a truth table for the expression !A && !B.
 
 |   !A   |   !B    |  A     |   B   |   !A && !B
-|!true   | !false  | false  | false |  false
-|!true   | !true   |  false |  false|  false
-|!false  | !false  |  true  |  true |  true 
-|!false  | !true   |   true | false |  false                
+|!true   | !false  | false  | true  |  false
+|!true   | !true   | false  | false |  false
+|!false  | !false  |  true  | true  |  true 
+|!false  | !true   |  true  | false |  false                
 
 For reference, here is a truth table for the expression A && !B:
 
@@ -138,8 +136,10 @@ PROBLEM 5:
 
 Write a step-by-step evaluation for the following expression (remember order of operations): 2 + 3 * 2 + 1. 
 Solution:
-3*2 = 6
-6 + 2 + 1 = 9
+2 + 3 * 2 + 1
+    2 + 6 + 1
+        8 + 1 
+            9
 
 For reference, here is a exp of a step-by-step evaluation:
 1 + 2 + 3 + 4
@@ -157,9 +157,9 @@ PROBLEM 6:
 
 Write a step-by-step evaluation for the following expression (remember order of operations): 4 / 2 + 8 / 4.
 Solution:
-4/2 = 2
-8/4 = 2
-2 + 2 = 4
+4 / 2 + 8 / 4
+        2 + 2
+            4
 
 
 ******************/
@@ -183,8 +183,10 @@ catterpillar
 PROBLEM 8:
 
 Write a step-by-step evaluation for the following expression: 2 * 4 === 8 && 'car' + 'pool' === 'carpool'.
-2*4 = 8, 2 * 4 === true
-car + pool = carpool, carpool === carpool, which equals true
+
+2 * 4 === 8 && 'car' + 'pool' === 'carpool'
+8 === 8 && 'carpool' === 'carpool'
+true && true
 
 
 ******************/
